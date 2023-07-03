6930c9ed27cdcd430b469f62739ee0abe43a0e5a
joetech12 <ifeanyi4umeh@gmail.com>
2023-07-03 18:08:52 +0100

    Added how it works section

--- components/HomePage/HowItWorks/index.js
+++ components/HomePage/HowItWorks/index.js
@@ -1,7 +1,10 @@
 import { titleFont } from '@/utils/fonts';
 import React from 'react';
 import { MdPayments, MdFreeCancellation } from 'react-icons/md';
-import { BiTask, BiWorld } from 'react-icons/bi';
+import { RiCustomerService2Fill } from 'react-icons/ri';
+import { FaArrowRightLong } from 'react-icons/fa6';
+import { IoMailUnreadOutline } from 'react-icons/io5';
+import { FaFileSignature } from 'react-icons/fa';
 
 const HowItWorks = () => {
   return (
@@ -13,50 +16,60 @@ const HowItWorks = () => {
           <h3
             className={`${titleFont.variable} font-sans2 text-3xl lg:text-4xl font-extrabold flex-col items-center justify-center`}
           >
-            <p className="">Our Services</p>{' '}
+            <p className="">How It Works</p>{' '}
             <div className="w-full bg-primary/20 h-[20px] mt-[-12px] "></div>
           </h3>
         </div>
 
-        {/* Services Cards */}
+        {/* How it works */}
         <div className="py-[0px] px-[20px] lg:px-[100px] xl:px-[20px] grid md:grid-cols-2  lg:grid-cols-2 xl:grid-cols-4 justify-center gap-[20px]">
-          <div className="flex flex-col space-y-[10px]">
-            <MdPayments className="text-[40px] md:text-[50px] text-secondary" />
-            <h3 className="text-[18px] md:text-[23px] font-bold leading-[25px]">
-              Multiple Payment Options.
-            </h3>
-            <p className="">Cash payment on-site or</p>
-            <img
-              src="/payment_methods.png"
-              alt=""
-              className="h-[20px] md:h-[40px] w-fit"
-            />
-          </div>
-
-          <div className="flex flex-col space-y-[10px]">
-            <BiTask className="text-[40px] md:text-[50px] text-secondary" />
-            <h3 className="text-[18px] md:text-[23px] font-bold leading-[25px]">
-              Same & next day bookings
-            </h3>
-            <p className="">Last minute move? We got you!</p>
-          </div>
-
-          <div className="flex flex-col space-y-[10px]">
-            <MdFreeCancellation className="text-[40px] md:text-[50px] text-secondary" />
-            <h3 className="text-[18px] md:text-[23px] font-bold leading-[25px]">
-              Free cancellation
-            </h3>
-            <p className="">
-              Cancel your booking for free or reschedule it for another time.
+          <div className="flex flex-col space-y-[10px] items-center group relative">
+            <FaArrowRightLong className="text-[40px] md:text-[50px] text-secondary absolute right-[-35px] top-[20%] hidden md:flex" />
+            <div className="bg-secondary w-[100px] h-[100px] rounded-full flex justify-center items-center relative group-hover:scale-[1.15] duration-200">
+              <RiCustomerService2Fill className="text-[40px] md:text-[50px] text-white" />
+              <div className="flex justify-center items-center bg-primary rounded-full h-[50px] w-[50px] absolute top-[-10px] left-[-10px]">
+                <p className="text-white text-[20px]">1</p>
+              </div>
+            </div>
+            <p className="text-center text-[18px] px-[70px] lg:px-[10px]">
+              Get a Quote and discuss your future move with our specialist
             </p>
           </div>
-
-          <div className="flex flex-col space-y-[10px]">
-            <BiWorld className="text-[40px] md:text-[50px] text-secondary" />
-            <h3 className="text-[18px] md:text-[23px] font-bold leading-[25px]">
-              Nationwide coverage
-            </h3>
-            <p className="">We cover the entire UK.</p>
+          <div className="flex flex-col space-y-[10px] items-center group relative">
+            <FaArrowRightLong className="text-[40px] md:text-[50px] text-secondary absolute right-[-35px] top-[20%] hidden xl:flex" />
+            <div className="bg-secondary w-[100px] h-[100px] rounded-full flex justify-center items-center relative group-hover:scale-[1.15] duration-200">
+              <IoMailUnreadOutline className="text-[40px] md:text-[50px] text-white" />
+              <div className="flex justify-center items-center bg-primary rounded-full h-[50px] w-[50px] absolute top-[-10px] left-[-10px]">
+                <p className="text-white text-[20px]">2</p>
+              </div>
+            </div>
+            <p className="text-center text-[18px] px-[70px] lg:px-[10px]">
+              Schedule your move and receive Confirmation via email
+            </p>
+          </div>
+          <div className="flex flex-col space-y-[10px] items-center group relative">
+            <FaArrowRightLong className="text-[40px] md:text-[50px] text-secondary absolute right-[-35px] top-[20%] hidden md:flex" />
+            <div className="bg-secondary w-[100px] h-[100px] rounded-full flex justify-center items-center relative group-hover:scale-[1.15] duration-200">
+              <FaFileSignature className="text-[40px] md:text-[50px] text-white" />
+              <div className="flex justify-center items-center bg-primary rounded-full h-[50px] w-[50px] absolute top-[-10px] left-[-10px]">
+                <p className="text-white text-[20px]">3</p>
+              </div>
+            </div>
+            <p className="text-center text-[18px] px-[70px] lg:px-[10px]">
+              On move day, just sign the contract. We’ll do the rest.
+            </p>
+          </div>
+          <div className="flex flex-col space-y-[10px] items-center group relative">
+            {/* <FaArrowRightLong className="text-[40px] md:text-[50px] text-secondary absolute right-[-35px] top-[20%] animate-bounce" /> */}
+            <div className="bg-secondary w-[100px] h-[100px] rounded-full flex justify-center items-center relative group-hover:scale-[1.15] duration-200">
+              <IoMailUnreadOutline className="text-[40px] md:text-[50px] text-white" />
+              <div className="flex justify-center items-center bg-primary rounded-full h-[50px] w-[50px] absolute top-[-10px] left-[-10px]">
+                <p className="text-white text-[20px]">4</p>
+              </div>
+            </div>
+            <p className="text-center text-[18px] px-[70px] lg:px-[50px]">
+              Pay your bill and enjoy your new place
+            </p>
           </div>
         </div>
       </div>

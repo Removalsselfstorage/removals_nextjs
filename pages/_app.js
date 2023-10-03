ad72958a04d1772cb8b12387ab6bbddc4096b99e
Joetech12 <ifeanyi4umeh@gmail.com>
2023-10-03 14:50:04 +0100

    removed the loader in _app page

--- pages/_app.js
+++ pages/_app.js
@@ -37,26 +37,26 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps: { ...pageProps } }) {
   const [loading, setLoading] = useState(false);
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const handleStart = () => {
-      setLoading(true);
-    };
-    const handleComplete = () => {
-      setLoading(false);
-    };
+  // useEffect(() => {
+  //   const handleStart = () => {
+  //     setLoading(true);
+  //   };
+  //   const handleComplete = () => {
+  //     setLoading(false);
+  //   };
 
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
+  //   router.events.on("routeChangeStart", handleStart);
+  //   router.events.on("routeChangeComplete", handleComplete);
+  //   router.events.on("routeChangeError", handleComplete);
 
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  }, [router]);
+  //   return () => {
+  //     router.events.off("routeChangeStart", handleStart);
+  //     router.events.off("routeChangeComplete", handleComplete);
+  //     router.events.off("routeChangeError", handleComplete);
+  //   };
+  // }, [router]);
 
   return (
     <>
@@ -69,7 +69,7 @@ export default function App({ Component, pageProps: { ...pageProps } }) {
           {/* <QueryClientProvider client={queryClient}> */}
           <AuthProvider>
             <div className="">
-              {loading && <Loader1 />}
+              {/* {loading && <Loader1 />} */}
               <Component {...pageProps} />
             </div>
             <Toaster
